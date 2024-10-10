263eae5c390fedb965bf8ce380cc3c3c4cd3784b
Acosta Juan <61852434+acostaljc@users.noreply.github.com>
2024-10-10 14:17:47 -0700

    segunda subida me equivoque en la ruta inicial

---
+++ ssaap/src/main/java/com/example/sseproduct/SsaapApplication.java
@@ -0,0 +1,13 @@
+package com.example.sseproduct;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SsaapApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SsaapApplication.class, args);
+    }
+
+}
