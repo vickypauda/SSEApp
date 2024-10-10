92a416a0ec36437b3e8be140117a4bd773757ce9
vickypauda <119009526+vickypauda@users.noreply.github.com>
2024-10-10 08:50:31 -0700

    SSE

---
+++ ssaap/src/main/java/com/example/ssaap/SsaapApplication.java
@@ -0,0 +1,13 @@
+package com.example.ssaap;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SsaapApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SsaapApplication.class, args);
+    }
+
+}
---
+++ ssaap/src/test/java/com/example/ssaap/SsaapApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.ssaap;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SsaapApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
